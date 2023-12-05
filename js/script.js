a98a96560790656ba8778e737c79ec188cd46c4c
PaoloTarcisio <paoloc50@gmail.com>
2023-12-05 17:15:24 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 const ContenitoreCarte = document.querySelector('.card-container');
 
-
 for (let i = 0; i < 100; i++) {
 
     const NewCard = document.createElement('div');
-    NewCard.className = "card";
-    NewCard.append (i + 1);
+    let NumeroCarta = (i + 1);
+    NewCard.className = "my-card text-center bg-card m-3 p-3";
+    NewCard.append (NumeroCarta);
+    
     ContenitoreCarte.append(NewCard);
+
+    if ((NumeroCarta % 3) == 0) {
+        NewCard.innerHTML = "Fizz";
+        NewCard.classList.remove("bg-card");
+        NewCard.classList.add("bg-fizz");
+        
+    }
+
+    if ((NumeroCarta % 5) == 0) {
+        NewCard.innerHTML = "Buzz";
+        NewCard.classList.remove("bg-card");
+        NewCard.classList.add("bg-fizz");
+    }
+
+    if (((NumeroCarta % 3) == 0) && ((NumeroCarta % 5) == 0)) {
+        NewCard.innerHTML = "FizzBuzz";
+        NewCard.classList.remove("bg-card");
+        NewCard.classList.add("bg-fizzbuzz");
+    }
 }
 
 
